73b6d50c8f97a3eb72b887e4285c500233f60623
찬옥 <chanok9417@kakao.com>
2020-05-18 14:14:04 +0900

    200518-1414

--- pages/_document.js
+++ pages/_document.js
@@ -25,11 +25,6 @@ class CustomDocument extends Document {
           `
             }}
           />
-          {/* <script
-            data-ad-client="ca-pub-4039783749152993"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          /> */}
           <link rel="shortcut icon" href="/favicon.ico" />
           <link
             rel="preload"
